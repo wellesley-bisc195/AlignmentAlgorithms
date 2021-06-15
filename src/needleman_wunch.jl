2972eb4cfb8e06b1bf2b60f7a5429861422cf6d6
Jen Enriquez <jjenriquez009@gmail.com>
2021-06-15 19:47:18 -0400

    added to nwscore

--- src/needleman_wunch.jl
+++ src/needleman_wunch.jl
@@ -3,10 +3,14 @@
 """
     nwscore(base1, base2)
 
-Your documentation here.
+Accepts two bases and returns the correct score based on Needleman Wunch paper
 """
 function nwscore(base1::Char, base2::Char)
-    
+    if(base1 == base2)
+        return +1
+
+    else
+        return -1
 end
 
 nwscore(base::Char, ::Nothing) = -1
