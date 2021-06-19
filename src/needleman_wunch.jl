1543885b036d331e7cab3b44bb586c92a6de86c3
Deniz Uzun <du1@wellesley.edu>
2021-06-19 01:34:25 -0700

    retry

--- src/needleman_wunch.jl
+++ src/needleman_wunch.jl
@@ -11,9 +11,6 @@ function nwscore(base1::Char, base2::Char; match=1, mismatch=-1)
         end
         if base1 != base2
             return -1
-        end
-        if isa kwarg 
-            return match || mismatch
     end
 nwscore(base::Char, ::Nothing) = -1
 nwscore(::Nothing, base::Char) = nwscore(base, nothing)
