eb552f565be83989bf1819866926307b2ea4c25d
Deniz Uzun <du1@wellesley.edu>
2021-06-20 18:17:00 -0700

    attempt

--- src/needleman_wunch.jl
+++ src/needleman_wunch.jl
@@ -6,16 +6,12 @@
 Your documentation here.
 """
 function nwscore(base1::Char, base2::Char; match, mismatch)
-        if base1 == base2
+        if base1 == base2 || match == true
             return 1
         end
-        if base1 != base2
+        if base1 != base2 || mismatch == true
             return -1
         end
-        if match == true
-            return match
-        if mismatch == true
-            return mismatch
     end
 nwscore(base::Char, ::Nothing) = -1
 nwscore(::Nothing, base::Char) = nwscore(base, nothing)
