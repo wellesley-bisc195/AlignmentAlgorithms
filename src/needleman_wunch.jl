105d8f3b201248560f9941457b0da3ef4c1a94a0
Deniz Uzun <du1@wellesley.edu>
2021-06-20 18:11:10 -0700

    add edits

--- src/needleman_wunch.jl
+++ src/needleman_wunch.jl
@@ -5,19 +5,18 @@
 
 Your documentation here.
 """
-function nwscore(base1::Char, base2::Char; match = nothing, mismatch = nothing)
-        kwargs_dict = [key=>value for (key, value) in kwargs]
-        if match == nothing && mismatch == nothing && base1 == base2
-           return 1
-        elseif  match == nothing && mismatch == nothing && base1 != base2
-            return -1
-        elseif match != nothing
-            return match
-        elseif mismatch != nothing
-            return mismatch
+function nwscore(base1::Char, base2::Char; match, mismatch)
+        if base1 == base2
+            return 1
         end
+        if base1 != base2
+            return -1
+        end
+        if isa match 
+            return match
+        if isa mismatch
+            return match
     end
 nwscore(base::Char, ::Nothing) = -1
 nwscore(::Nothing, base::Char) = nwscore(base, nothing)
-
 nwscore(::Nothing, ::Nothing) = throw(ArgumentError("Score for two gaps is not defined"))
