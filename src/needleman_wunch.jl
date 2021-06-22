60a74f59b33bc664e8519e42de926da2c7c7a515
achua1 <69883692+achua1@users.noreply.github.com>
2021-06-22 20:59:04 +0000

    doc string added to needleman_wunch.jl

--- src/needleman_wunch.jl
+++ src/needleman_wunch.jl
@@ -3,7 +3,19 @@
 """
     nwscore(base1, base2)
 
-Your documentation here.
+Determine Needleman Wunsch score for DNA sequence alignment
+
+Examples
+≡≡≡≡≡≡≡≡≡≡
+
+julia> nwscore('A', 'A')
+1
+
+julia> nwscore('A', 'T')
+-1
+
+julia> nwscore('A', nothing; gap=-4)
+-4
 """
 function nwscore(base1::Char, base2::Char; match=1, mismatch=-1, gap=-1) 
     if base1 == base 2
