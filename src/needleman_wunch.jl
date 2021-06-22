e28cec9fadba1a8d8faf9b95e860ae95e7779036
Ivy Ho <iho@wellesley.edu>
2021-06-22 01:37:40 -0400

    lab3 part1 completed

--- src/needleman_wunch.jl
+++ src/needleman_wunch.jl
@@ -1,15 +1,43 @@
 # Labs 3 & 4
 
 """
-    nwscore(base1, base2)
+    nwscore(base1, base2; match = 1, mismatch = -1, gap = -1) -> Int
 
-Your documentation here.
+Returns a match, mismatch, or gap score depending on the type and value of base1 and base2.
+The type of base1 and base2 must be Char or nothing, else a type error will be thrown.
+If base1 and base2 are equivalent, that consititutes a match.
+If base1 and base2 are not equivalent but both are of type Char, that constitutes a mismatch.
+If either base1 or base2 is of type Char and the other is of type nothing, that constitutes a gap.
+If both base1 and base2 are of type nothing, nwscore will throw an ArgumentError.
+The kwargs match, mismatch, and gap have a default value of 1, -1, and -1 respectively.
+
+Examples
+≡≡≡≡≡≡≡≡≡≡
+
+nwscore('A', 'A') -> 1
+nwscore('A', 'A'; match = 2) -> 2
+nwscore('A', 'A'; mismatch = -2) -> 1
+nwscore('A', 'A'; match = 2, mismatch = -2, gap = -2) -> 2
+
+nwscore('A', 'C') -> -1
+nwscore('A', 'C'; mismatch = -2) -> -2
+nwscore('A', 'C'; match = 2) -> -1
+nwscore('A', 'C'; match = 2, mismatch = -2, gap = -2) -> -2
+
+nwscore('A', nothing) -> -1
+nwscore(nothing, 'C'; gap = -2) -> -2
+nwscore('A', nothing; match = 2, mismatch = -2, gap = -2) -> -2
+nwscore(nothing, nothing; match = 2, mismatch = -2, gap = -2) -> ArgumentError
 """
-function nwscore(base1::Char, base2::Char)
-    
+function nwscore(base1::Char, base2::Char; match = 1, mismatch = -1, gap = -1)
+    return base1 == base2 ? match : mismatch
 end
 
-nwscore(base::Char, ::Nothing) = -1
-nwscore(::Nothing, base::Char) = nwscore(base, nothing)
+nwscore(base::Char, ::Nothing; match = 1, mismatch = -1, gap = -1) = gap
+nwscore(::Nothing, base::Char; match = 1, mismatch = -1, gap = -1) = nwscore(base, nothing; gap = gap)
 
-nwscore(::Nothing, ::Nothing) = throw(ArgumentError("Score for two gaps is not defined"))
+nwscore(::Nothing, ::Nothing; match = 1, mismatch = -1, gap = -1) = throw(ArgumentError("Score for two gaps is not defined"))
+
+function nwalign()
+    # aligner
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -9,12 +9,14 @@ using Test
     for base in bases
         @test nwscore(base, base) == 1
         @test nwscore(base, base; match = 2) == 2
+        @test nwscore(base, base; match = 1, gap = -2) == 1
     end
 
     # Test mismatch score
     for (b1, b2) in combinations(bases, 2)
         @test nwscore(b1, b2) == -1
         @test nwscore(b1, b2; mismatch = -2) == -2
+        @test nwscore(b1, b2; mismatch = -1, gap = -2) == -1
     end
     
     # Test gap score
@@ -23,6 +25,30 @@ using Test
         @test nwscore(nothing, base) == -1
         @test nwscore(base, nothing; gap = -2) == -2
         @test nwscore(nothing, base; gap = -2) == -2
+        @test nwscore(base, nothing; match = 2, gap = -2) == -2
+        @test nwscore(nothing, base; mismatch = -1, gap = -2) == -2
+        @test nwscore(base, nothing; match = 2, mismatch = -1, gap = -2) == -2
+        @test nwscore(nothing, base; match = 2, mismatch = -1, gap = -2) == -2
     end
+
+    # Test error
+    @test_throws ArgumentError nwscore(nothing, nothing)
+    @test_throws ArgumentError nwscore(nothing, nothing; gap = -2)
+    @test_throws ArgumentError nwscore(nothing, nothing; match = 2, mismatch = -3)
+    @test_throws ArgumentError nwscore(nothing, nothing; match = 3, mismatch = -3, gap = -2)
+
+end
+
+# Test nwalign
+@testset "nwalign" begin
+    seq1 = "ACGAT"
+    seq2 = "AGGT"
+    bestAlignment = Set{String}([("ACGAT", "A-GGT"), ("ACGAT", "AGG-T")])
+
+    @test nwalign(seq1, seq2) == bestAlignment, 1
+    @test nwalign(seq1, seq2; match = 2) == bestAlignment, 4
+    @test nwalign(seq1, seq2; gap = -2) == bestAlignment, 0
+    @test nwalign(seq1, seq2; match = 2, mismatch = -2) == bestAlignment, 3
+    @test nwalign(seq1, seq2; match = 2, mismatch = -2, gap = -2) == bestAlignment, 2
 
 end
